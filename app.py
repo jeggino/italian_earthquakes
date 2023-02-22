fe758c996e27777199fa3cc5a53e09cfc0e58fa5
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2023-02-22 15:44:00 +0100

    Create app.py

---
+++ app.py
@@ -0,0 +1,30 @@
+import streamlit as st
+from streamlit_option_menu import option_menu
+
+import folium
+from folium.plugins import Draw, Fullscreen, LocateControl
+from streamlit_folium import st_folium
+
+import pandas as pd
+import geopandas as gpd
+import datetime
+from datetime import date
+
+from streamlit_elements import elements, mui, html
+
+st.set_page_config(
+    page_title="Italian Earthquakes",
+    page_icon="🌍",
+    layout="wide",
+    
+)
+
+st.markdown(""" <style>
+#MainMenu {visibility: hidden;}
+footer {visibility: hidden;}
+</style> """, unsafe_allow_html=True)
+
+#---FUNCTIONS---
+def load_dataset():
+
+    return db.fetch().items
