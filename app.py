5a95447e7b9a0d8a21d9c554817b08c9fa18d686
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2023-02-22 16:23:58 +0100

    Update app.py

--- app.py
+++ app.py
@@ -26,6 +26,7 @@ footer {visibility: hidden;}
 @st.cache_data(experimental_allow_widgets=True)  # 👈 Set the parameter
 def get_data():
     df_raw = pd.read_csv("eartquakes_italy.csv")
+    st.slider('How old are you?', 0, 130, 25)
 #     magnitudo = st.slider(label="Magnitudo", min_value=0, max_value=6.2, value =(2.0, 5.0))
 #     mask_magnitudo = (df_raw['magnitudo_score'] >= magnitudo[0]) & (df_raw['magnitudo_score'] <= magnitudo[1])
 #     data = df_raw[mask_magnitudo]
