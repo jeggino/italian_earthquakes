f6f05253370e823313bebe1d4e9843e48b4b0692
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2023-02-23 17:07:51 +0100

    Update app.py

--- app.py
+++ app.py
@@ -79,8 +79,8 @@ with left_2:
     
     source = filtered_data.groupby("reg_name",as_index=False).size()
     chart = alt.Chart(source).mark_bar().encode(
-        x=alt.X('size:Q', axis=None),
-        y=alt.Y('reg_name:N', sort='-x', axis=None)
+        x=alt.X('size:Q', title=None),
+        y=alt.Y('reg_name:N', sort='-x', title=None)
     )
 
     st.altair_chart(chart, use_container_width=True, theme="streamlit")
