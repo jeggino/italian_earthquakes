efb7c1d7601194a19c20c6c4fa6c90bc91403160
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2023-02-23 10:29:06 +0100

    Update app.py

--- app.py
+++ app.py
@@ -28,7 +28,8 @@ footer {visibility: hidden;}
 @st.cache_data(experimental_allow_widgets=True)  # 👈 Set the parameter
 def get_data():
     
-    df_raw = pd.read_csv("https://webservices.ingv.it/fdsnws/event/1/query?starttime=2020-02-22T00%3A00%3A00&endtime=2023-02-23T23%3A59%3A59&minmag=2&maxmag=10&mindepth=-10&maxdepth=1000&minlat=-90&maxlat=90&minlon=-180&maxlon=180&minversion=100&orderby=time-asc&format=text&limit=10000",sep="|")[['Time', 'Latitude', 'Longitude', 'Depth/Km', 'Magnitude','EventLocationName']]
+    df_raw = pd.read_csv("https://webservices.ingv.it/fdsnws/event/1/query?starttime=2021-02-22T00%3A00%3A00&endtime=2023-02-23T23%3A59%3A59&minmag=2&maxmag=10&mindepth=-10&maxdepth=1000&minlat=-90&maxlat=90&minlon=-180&maxlon=180&minversion=100&orderby=time-asc&format=text&limit=10000",
+                        sep="|")[['Time', 'Latitude', 'Longitude', 'Depth/Km', 'Magnitude']]
     df_raw["Time"] = df_raw["Time"].str.split("T",expand=True)[0]
     
     values_magnitude = st.slider(
