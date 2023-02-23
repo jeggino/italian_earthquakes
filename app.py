cd4eb7e17cae99b1adbfdb761df5a043244271c9
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2023-02-23 07:36:22 +0100

    Update app.py

--- app.py
+++ app.py
@@ -28,7 +28,7 @@ footer {visibility: hidden;}
 @st.cache_data(experimental_allow_widgets=True)  # 👈 Set the parameter
 def get_data():
     
-    df_raw = pd.read_csv("eartquakes_italy.csv")
+    df_raw = pd.read_csv('Italian_Catalogue.csv')
     values_magnitudo = st.slider(
     'Magnitudo',
     0.0, 6.2, (2.0, 5.0))
@@ -37,79 +37,79 @@ def get_data():
     'Deepness',
     0, 644, (0, 644))
     
-    magnitudo_mask = ((df_raw["magnitudo_score"]>=values_magnitudo[0]) & (df_raw["magnitudo_score"]<=values_magnitudo[1]))
-    deepness_mask = ((df_raw["Profondità"]>=values_deepness[0]) & (df_raw["Profondità"]<=values_deepness[1]))
+    magnitudo_mask = ((df_raw["Magnitude"]>=values_magnitudo[0]) & (df_raw["Magnitude"]<=values_magnitudo[1]))
+    deepness_mask = ((df_raw["Depth/Km"]>=values_deepness[0]) & (df_raw["Depth/Km"]<=values_deepness[1]))
     
     filtered_data = df_raw[magnitudo_mask & deepness_mask]
     
     return filtered_data
 
-@st.cache_data() 
-def get_municipalities():
-    df_municipalities = gpd.read_file('https://raw.githubusercontent.com/openpolis/geojson-italy/master/geojson/limits_IT_municipalities.geojson')
-    df_municipalities = df_municipalities[['name','prov_name','reg_name','geometry']].rename(columns={'name':'mun_name'})
-    return df_municipalities
+# @st.cache_data() 
+# def get_municipalities():
+#     df_municipalities = gpd.read_file('https://raw.githubusercontent.com/openpolis/geojson-italy/master/geojson/limits_IT_municipalities.geojson')
+#     df_municipalities = df_municipalities[['name','prov_name','reg_name','geometry']].rename(columns={'name':'mun_name'})
+#     return df_municipalities
 
-@st.cache_data() 
-def get_dataset_2():
-    df_raw = pd.read_csv('Italian_Catalogue.csv')
-    form = "%Y-%m-%d %H:%M:%S"
-    df_raw['Time'] = pd.to_datetime(df_raw['Time'], format=form)
-    geometry = [Point(xy) for xy in zip(df_raw.Longitude, df_raw.Latitude)]
-    geo_df = gpd.GeoDataFrame(df_raw, geometry=geometry,crs="EPSG:4326")
+# @st.cache_data() 
+# def get_dataset_2():
+#     df_raw = pd.read_csv('Italian_Catalogue.csv')
+#     form = "%Y-%m-%d %H:%M:%S"
+#     df_raw['Time'] = pd.to_datetime(df_raw['Time'], format=form)
+#     geometry = [Point(xy) for xy in zip(df_raw.Longitude, df_raw.Latitude)]
+#     geo_df = gpd.GeoDataFrame(df_raw, geometry=geometry,crs="EPSG:4326")
     
-    return geo_df
+#     return geo_df
     
 
-@st.cache_data() 
-def get_pointInPoly_municipalities():
-    pointInPoly_municipalities = gpd.sjoin(get_dataset_2(), get_municipalities(), op='within').reset_index(drop=True).drop_duplicates()
-    pointInPoly_municipalities['date'] = pointInPoly_municipalities['Time'].dt.date
-    return pointInPoly_municipalities
+# @st.cache_data() 
+# def get_pointInPoly_municipalities():
+#     pointInPoly_municipalities = gpd.sjoin(get_dataset_2(), get_municipalities(), op='within').reset_index(drop=True).drop_duplicates()
+#     pointInPoly_municipalities['date'] = pointInPoly_municipalities['Time'].dt.date
+#     return pointInPoly_municipalities
     
-@st.cache_data() 
-def get_heatmap():    
+# @st.cache_data() 
+# def get_heatmap():    
     
-    df_HeatMap = get_pointInPoly_municipalities()[['date', 'Latitude', 'Longitude']].sort_values('date').reset_index(drop=True)
-    df_HeatMap['date'] = df_HeatMap['date'].astype(str)
-    lat_long_list = []
-    for i in df_HeatMap.date.unique():
-        temp=[]
-        for index, instance in df_HeatMap[df_HeatMap['date'] == i].iterrows():
-            temp.append([instance['Latitude'],instance['Longitude']])
-        lat_long_list.append(temp)
+#     df_HeatMap = get_pointInPoly_municipalities()[['date', 'Latitude', 'Longitude']].sort_values('date').reset_index(drop=True)
+#     df_HeatMap['date'] = df_HeatMap['date'].astype(str)
+#     lat_long_list = []
+#     for i in df_HeatMap.date.unique():
+#         temp=[]
+#         for index, instance in df_HeatMap[df_HeatMap['date'] == i].iterrows():
+#             temp.append([instance['Latitude'],instance['Longitude']])
+#         lat_long_list.append(temp)
         
-    # create a map
-    centroid = get_municipalities().centroid
+#     # create a map
+#     centroid = get_municipalities().centroid
 
 
-    m = folium.Map(location=[centroid.y.mean(), centroid.x.mean()], zoom_start=6,  
-                   tiles='cartodbdark_matter',
-                   attr='&copy'
-                  )
+#     m = folium.Map(location=[centroid.y.mean(), centroid.x.mean()], zoom_start=6,  
+#                    tiles='cartodbdark_matter',
+#                    attr='&copy'
+#                   )
 
 
 
-    HeatMapWithTime(lat_long_list,
-                    index=df_HeatMap.date.unique().tolist(),
-                    name='heatmap',
-                    overlay=False,
-                    radius=15,
-                    auto_play=True,
-                    speed_step=1,
-                    position='bottomright',
-                    display_index=True
-    ).add_to(m)
+#     HeatMapWithTime(lat_long_list,
+#                     index=df_HeatMap.date.unique().tolist(),
+#                     name='heatmap',
+#                     overlay=False,
+#                     radius=15,
+#                     auto_play=True,
+#                     speed_step=1,
+#                     position='bottomright',
+#                     display_index=True
+#     ).add_to(m)
 
-    #fullscreen
-    folium.plugins.Fullscreen(position='topleft', title='Full Screen', title_cancel='Exit Full Screen', force_separate_button=True,).add_to(m)
+#     #fullscreen
+#     folium.plugins.Fullscreen(position='topleft', title='Full Screen', title_cancel='Exit Full Screen', force_separate_button=True,).add_to(m)
 
-    return m
+#     return m
 
 
 
 st.dataframe(data=get_data(), use_container_width=True)
-st.dataframe(data=get_pointInPoly_municipalities(), use_container_width=True)
+# st.dataframe(data=get_pointInPoly_municipalities(), use_container_width=True)
 # st_folium(get_heatmap())
 
 
