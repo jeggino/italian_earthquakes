a0a4de1e53ff35ea18956fe7633cb17634219265
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2023-02-23 14:48:51 +0100

    Update app.py

--- app.py
+++ app.py
@@ -60,11 +60,21 @@ st.pydeck_chart(pdk.Deck(
     ),
     layers=[
         pdk.Layer(
-           'ScatterplotLayer',
-           data=filtered_data,
-           get_position='[Longitude, Latitude]',
-           radius=2000,
-#            get_color='[200, 30, 0, 160]'
+            "ScatterplotLayer",
+            data=filtered_data,
+            pickable=True,
+            opacity=0.8,
+            stroked=True,
+            filled=True,
+            radius_scale=6,
+            radius_min_pixels=1,
+            radius_max_pixels=100,
+            line_width_min_pixels=1,
+            get_position='[Longitude, Latitude]',
+            get_radius="Magnitude",
+            get_fill_color=[255, 140, 0],
+            get_line_color=[0, 0, 0],
+           
         )
     ],
 ))
