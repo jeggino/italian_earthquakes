68b4a72f85c522dc15381215825d377d28f9b757
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2023-02-23 19:16:29 +0100

    Update app.py

--- app.py
+++ app.py
@@ -124,7 +124,7 @@ with right:
         initial_view_state=pdk.ViewState(
             latitude=filtered_data["Latitude"].mean(),
             longitude=filtered_data["Longitude"].mean(),
-            zoom=5,
+            zoom=4,
             pitch=0,
         ),
         layers=[
