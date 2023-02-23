d9cd08fecae7b1c07358310289f5667d6df2ed5e
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2023-02-23 14:09:38 +0100

    Update app.py

--- app.py
+++ app.py
@@ -40,8 +40,8 @@ def get_data():
     'Depth/Km',
     0, 644, (0, 644))
     
-    magnitudo_mask = ((df_raw["Magnitude"]>=values_magnitudo[0]) & (df_raw["Magnitude"]<=values_magnitudo[1]))
-    deepness_mask = ((df_raw["Depth/Km"]>=values_deepness[0]) & (df_raw["Depth/Km"]<=values_deepness[1]))
+    magnitudo_mask = ((df_raw["Magnitude"]>=magnitudo_mask[0]) & (df_raw["Magnitude"]<=magnitudo_mask[1]))
+    deepness_mask = ((df_raw["Depth/Km"]>=deepness_mask[0]) & (df_raw["Depth/Km"]<=deepness_mask[1]))
     
     filtered_data = df_raw[magnitudo_mask & deepness_mask]
     
