0e3877cc75ffcbe4a75e9a4d5dc656b0290b5b7a
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2023-02-24 07:47:00 +0100

    Update app.py

--- app.py
+++ app.py
@@ -33,8 +33,8 @@ st.set_page_config(
 def get_data():
     try:
         col_1,col_2 = st.columns(2)
-        starttime = col_1.date_input("Start time **World!**", value=datetime.date(2022, 7, 6), min_value=None, max_value=None, key=None, help=None, on_change=None, args=None, kwargs=None, disabled=False, label_visibility="visible")
-        endtime = col_2.date_input("End time", value=None, min_value=None, max_value=None, key=None, help=None, on_change=None, args=None, kwargs=None, disabled=False, label_visibility="visible")
+        starttime = col_1.date_input("**Start time**", value=datetime.date(2022, 7, 6), min_value=None, max_value=None, key=None, help=None, on_change=None, args=None, kwargs=None, disabled=False, label_visibility="visible")
+        endtime = col_2.date_input("**End time**", value=None, min_value=None, max_value=None, key=None, help=None, on_change=None, args=None, kwargs=None, disabled=False, label_visibility="visible")
         df_raw = pd.read_csv(f"https://webservices.ingv.it/fdsnws/event/1/query?starttime={str(starttime)}T00%3A00%3A00&{str(endtime)}=2023-02-23T23%3A59%3A59&minmag=2&maxmag=10&mindepth=-10&maxdepth=1000&minlat=35&maxlat=49&minlon=5&maxlon=20&minversion=100&orderby=time-asc&format=text&limit=10000",
                             sep="|")[['Time', 'Latitude', 'Longitude', 'Depth/Km', 'Magnitude']]
         df_raw["Time"] = df_raw["Time"].str.split("T",expand=True)[0]
@@ -69,8 +69,8 @@ with left:
     df = get_data()
     left_1,  right_2 = st.columns([2,2], gap="medium")
 
-    values_magnitude = left_1.slider('Magnitude',int(df.Magnitude.min()), int(df.Magnitude.max()), (int(df.Magnitude.min()), int(df.Magnitude.max())))
-    values_deepness = right_2.slider('Depth/Km',int(df["Depth/Km"].min()), int(df["Depth/Km"].max()), (int(df["Depth/Km"].min()), int(df["Depth/Km"].max())))    
+    values_magnitude = left_1.slider('**Magnitude**',int(df.Magnitude.min()), int(df.Magnitude.max()), (int(df.Magnitude.min()), int(df.Magnitude.max())))
+    values_deepness = right_2.slider('**Depth/Km**',int(df["Depth/Km"].min()), int(df["Depth/Km"].max()), (int(df["Depth/Km"].min()), int(df["Depth/Km"].max())))    
 
     magnitudo_mask = ((df["Magnitude"]>=values_magnitude[0]) & (df["Magnitude"]<=values_magnitude[1]))
     deepness_mask = ((df["Depth/Km"]>=values_deepness[0]) & (df["Depth/Km"]<=values_deepness[1]))
@@ -96,8 +96,8 @@ with right:
        }
     }
     
-    with st.expander("🗺️", expanded=True):
-        tab1, tab2 = st.tabs(["Points", "Heatmap"])
+    with st.expander("**Map** 🗺️", expanded=True):
+        tab1, tab2 = st.tabs(["*Points*", "*Heatmap*"])
 
         tab1.pydeck_chart(pdk.Deck(
             map_provider="mapbox", 
