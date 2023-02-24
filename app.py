ebbeea8dca5e1731740b02d39e1f1ad98067e065
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2023-02-24 08:06:52 +0100

    Update app.py

--- app.py
+++ app.py
@@ -159,3 +159,20 @@ with right:
                 )
         ],
     ), use_container_width=True)
+        
+        
+def get_user_name():
+    return 'John'
+
+with st.echo():
+    # Everything inside this block will be both printed to the screen
+    # and executed.
+
+    def get_punctuation():
+        return '!!!'
+
+    greeting = "Hi there, "
+    value = get_user_name()
+    punctuation = get_punctuation()
+
+    st.write(greeting, value, punctuation)
