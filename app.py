338a140338a6c8b371f924b847b0297041337165
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2023-02-25 11:52:19 +0100

    Update app.py

--- app.py
+++ app.py
@@ -225,9 +225,9 @@ with right:
                         ).add_to(m)
 
         #fullscreen
-        folium.plugins.Fullscreen(position='topleft', title='Full Screen', title_cancel='Exit Full Screen', force_separate_button=True,).add_to(m)
-
-        st_folium(m)
+#         folium.plugins.Fullscreen(position='topleft', title='Full Screen', title_cancel='Exit Full Screen', force_separate_button=True,).add_to(m)
+        with tab3:
+            st_folium(m)
     
     
                              
