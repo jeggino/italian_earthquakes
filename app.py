7a03603ea14ae02859de27e2c0581f07f834526e
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2023-03-06 10:34:18 +0100

    Update app.py

--- app.py
+++ app.py
@@ -46,20 +46,43 @@ from streamlit_chat import message
 import streamlit as st
 from streamlit_chat import message
 
-message("Welcome to Streamlit-Chat")
+st.header("Streamlit Chat - Demo")
+st.markdown("[Github](https://github.com/ai-yash/st-chat)")
 
-if 'message_history' not in st.session_state:
-    st.session_state.message_history = " "
+if 'generated' not in st.session_state:
+    st.session_state['generated'] = []
 
-for message_ in st.session_state.message_history:
-    message(message_,is_user=True) # display all the previous message
+if 'past' not in st.session_state:
+    st.session_state['past'] = []
 
-    placeholder = st.empty() # placeholder for latest message
-    input_ = st.text_input("you")
-    st.session_state.message_history.append(input_)
+def query(payload):
+	response = requests.post(API_URL, headers=headers, json=payload)
+	return response.json()
 
-with placeholder.container():
-    message( st.session_state.message_history[-1], is_user=True) # display the latest message
+def get_text():
+    input_text = st.text_input("You: ","Hello, how are you?", key="input")
+    return input_text 
+
+
+user_input = get_text()
+
+if user_input:
+    output = query({
+        "inputs": {
+            "past_user_inputs": st.session_state.past,
+            "generated_responses": st.session_state.generated,
+            "text": user_input,
+        },"parameters": {"repetition_penalty": 1.33},
+    })
+
+    st.session_state.past.append(user_input)
+    st.session_state.generated.append(output["generated_text"])
+
+if st.session_state['generated']:
+
+    for i in range(len(st.session_state['generated'])-1, -1, -1):
+        message(st.session_state["generated"][i], key=str(i))
+        message(st.session_state['past'][i], is_user=True, key=str(i) + '_user')
 
 #---FUNCTIONS---
 @st.cache_data(experimental_allow_widgets=True)  # 👈 Set the parameter
