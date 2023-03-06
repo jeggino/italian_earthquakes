48bee5bafb7aa6fe5d6db3e100f6691e7b6549bb
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2023-03-06 10:24:08 +0100

    Update app.py

--- app.py
+++ app.py
@@ -43,17 +43,20 @@ st.markdown(
 
 from streamlit_chat import message
 
+import streamlit as st
+from streamlit_chat import message
+
 message("Welcome to Streamlit-Chat")
 
-if "message_history" not in st.session_state:
-    st.session_state.message_history = None
+if ‘message_history’ not in st.session_state:
+    st.session_state.message_history = " "
 
 for message_ in st.session_state.message_history:
     message(message_,is_user=True) # display all the previous message
 
-placeholder = st.empty() # placeholder for latest message
-input_ = st.text_input("you")
-st.session_state.message_history.append(input_)
+    placeholder = st.empty() # placeholder for latest message
+    input_ = st.text_input("you")
+    st.session_state.message_history.append(input_)
 
 with placeholder.container():
     message( st.session_state.message_history[-1], is_user=True) # display the latest message
