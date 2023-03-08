4e646bccc4c1c0c024ea177553df4b414aec0e67
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2023-03-08 12:58:36 +0100

    Update app.py

--- app.py
+++ app.py
@@ -83,150 +83,157 @@ def get_data():
         st.stop()
 
 
-with st.container():
-    df = get_data()    
-    today = date.today()
-    years10  =date(today.year - 5, today.month, today.day)
-    starttime = st.sidebar.date_input("**Start time**", value=date(date.today().year, date.today().month, date.today().day-1), 
-                                      min_value=datetime.strptime(df.Time.min(),'%Y-%m-%d'), 
-                                      max_value=date.today(), label_visibility="visible")
-    endtime = st.sidebar.date_input("**End time**", value=None, 
-                                    min_value=datetime.strptime(df.Time.min(),'%Y-%m-%d'), 
-                                    max_value=date.today(), label_visibility="visible")
+df = get_data()
 
-    values_magnitude = st.sidebar.slider('**Magnitude**',int(df.Magnitude.min()), int(df.Magnitude.max()), (int(df.Magnitude.min()), int(df.Magnitude.max())))
-    values_deepness = st.sidebar.slider('**Depth/Km**',int(df["Depth/Km"].min()), int(df["Depth/Km"].max()), (int(df["Depth/Km"].min()), int(df["Depth/Km"].max())))    
+today = date.today()
+years10  =date(today.year - 5, today.month, today.day)
+starttime = st.sidebar.date_input("**Start time**", value=date(date.today().year, date.today().month, date.today().day-1), 
+                                  min_value=datetime.strptime(df.Time.min(),'%Y-%m-%d'), 
+                                  max_value=date.today(), label_visibility="visible")
+endtime = st.sidebar.date_input("**End time**", value=None, 
+                                min_value=datetime.strptime(df.Time.min(),'%Y-%m-%d'), 
+                                max_value=date.today(), label_visibility="visible")
+
+values_magnitude = st.sidebar.slider('**Magnitude**',int(df.Magnitude.min()), int(df.Magnitude.max()), (int(df.Magnitude.min()), int(df.Magnitude.max())))
+values_deepness = st.sidebar.slider('**Depth/Km**',int(df["Depth/Km"].min()), int(df["Depth/Km"].max()), (int(df["Depth/Km"].min()), int(df["Depth/Km"].max())))    
+
+
+time_mask = ((df['Time'] >= str(starttime) ) & (df['Time'] <= str(endtime)))
+magnitudo_mask = ((df["Magnitude"]>=values_magnitude[0]) & (df["Magnitude"]<=values_magnitude[1]))
+deepness_mask = ((df["Depth/Km"]>=values_deepness[0]) & (df["Depth/Km"]<=values_deepness[1]))
+filtered_data = df[magnitudo_mask & deepness_mask & time_mask]
+
+        
+selected = option_menu(
+    menu_title=None,
+    options=["Make an appointment", "Cancel an appointment"],
+    icons=["bi-journal-check", "bi-x-octagon-fill"],  # https://icons.getbootstrap.com/
+    orientation="horizontal",
+)
+
+if selected == "Make an appointment":
+
+    source_1 = filtered_data.groupby("reg_name",as_index=False).size()
+    chart_1 = alt.Chart(source_1).mark_bar().encode(
+        x=alt.X('size:Q', title="Number of earthquakes"),
+        y=alt.Y('reg_name:N', sort='-x', title="Region")
+    )
+
+    source_2 = filtered_data.groupby("prov_name",as_index=False).size().sort_values("size",ascending=False).reset_index(drop=True)[:20]
+    chart_2 = alt.Chart(source_2).mark_bar().encode(
+        x=alt.X('size:Q', title="Number of earthquakes"),
+        y=alt.Y('prov_name:N', sort='-x', title="Provinces")
+    )
+
+    sourc_3 = filtered_data.groupby("mun_name",as_index=False).size().sort_values("size",ascending=False).reset_index(drop=True)[:20]
+    chart_3 = alt.Chart(sourc_3).mark_bar().encode(
+        x=alt.X('size:Q', title="Number of earthquakes"),
+        y=alt.Y('mun_name:N', sort='-x', title="Municipalities")
+    )
+
+    with st.expander("**Charts** 📊", expanded=True):
+        tab1, tab2, tab3 = st.tabs(["*Regions*", "*Provinces*", "*Municipalities*"])
+
+        tab1.altair_chart(chart_1, use_container_width=True, theme=None)
+        tab2.altair_chart(chart_2, use_container_width=True, theme=None)
+        tab2.caption('This is a string that explains something above.')
+        tab3.altair_chart(chart_3, use_container_width=True, theme=None)
+        tab3.caption('This is a string that explains something above.')
+
+
+elif selected == "Cancel an appointment":
     
-    
-    time_mask = ((df['Time'] >= str(starttime) ) & (df['Time'] <= str(endtime)))
-    magnitudo_mask = ((df["Magnitude"]>=values_magnitude[0]) & (df["Magnitude"]<=values_magnitude[1]))
-    deepness_mask = ((df["Depth/Km"]>=values_deepness[0]) & (df["Depth/Km"]<=values_deepness[1]))
-    filtered_data = df[magnitudo_mask & deepness_mask & time_mask]
+    tooltip = {
+       "html": """
+       <b>Region:</b> {reg_name} <br />
+        <b>Province:</b> {prov_name} <br />
+        <b>Municipality:</b> {mun_name} <br />
+        <b>Date:</b> {Time} <br />
+        <b>Magnitude:</b> {Magnitude} <br />
+        <b>Depth:</b> {Depth/Km} Km
+        """,
+       "style": {
+            "backgroundColor": "steelblue",
+            "color": "white"
+       }
+    }
 
-    left,  right = st.columns([2,2], gap="medium")
+    with st.expander("**Map** 🌍", expanded=True):
+        tab1, tab2, tab3 = st.tabs(["*Points*", "*Heatmap*", "*Timelapse heatmap*"])
 
-    with left:
-        source_1 = filtered_data.groupby("reg_name",as_index=False).size()
-        chart_1 = alt.Chart(source_1).mark_bar().encode(
-            x=alt.X('size:Q', title="Number of earthquakes"),
-            y=alt.Y('reg_name:N', sort='-x', title="Region")
-        )
+        tab1.pydeck_chart(pdk.Deck(
+            map_provider="mapbox", 
+            map_style="road",
+            tooltip=tooltip,
+            initial_view_state=pdk.ViewState(
+                latitude=41.902782, 
+                longitude=12.496366,
+                zoom=4,
+                pitch=50,
+            ),
+            layers=[
+                pdk.Layer(
+                    "ScatterplotLayer",
+                    data=filtered_data,
+                    pickable=True,
+                    opacity=0.3,
+                    stroked=True,
+                    filled=True,
+                    radius_scale=10,
+                    radius_min_pixels=10,
+                    radius_max_pixels=100,
+                    line_width_min_pixels=1,
+                    get_position='[Longitude, Latitude]',
+                    get_radius="Magnitude",
+                    get_fill_color=[255, 140, 0],
+                    get_line_color=[0, 0, 0],
 
-        source_2 = filtered_data.groupby("prov_name",as_index=False).size().sort_values("size",ascending=False).reset_index(drop=True)[:20]
-        chart_2 = alt.Chart(source_2).mark_bar().encode(
-            x=alt.X('size:Q', title="Number of earthquakes"),
-            y=alt.Y('prov_name:N', sort='-x', title="Provinces")
-        )
+                )
+            ],
+        ), use_container_width=True)
 
-        sourc_3 = filtered_data.groupby("mun_name",as_index=False).size().sort_values("size",ascending=False).reset_index(drop=True)[:20]
-        chart_3 = alt.Chart(sourc_3).mark_bar().encode(
-            x=alt.X('size:Q', title="Number of earthquakes"),
-            y=alt.Y('mun_name:N', sort='-x', title="Municipalities")
-        )
+        #---HEATMAP---
+        m_1 = folium.Map(location=[41.902782, 12.496366],
+                        zoom_start=5)
 
-        with st.expander("**Charts** 📊", expanded=True):
-            tab1, tab2, tab3 = st.tabs(["*Regions*", "*Provinces*", "*Municipalities*"])
-
-            tab1.altair_chart(chart_1, use_container_width=True, theme=None)
-            tab2.altair_chart(chart_2, use_container_width=True, theme=None)
-            tab2.caption('This is a string that explains something above.')
-            tab3.altair_chart(chart_3, use_container_width=True, theme=None)
-            tab3.caption('This is a string that explains something above.')
-
-
-    with right:
-
-        tooltip = {
-           "html": """
-           <b>Region:</b> {reg_name} <br />
-            <b>Province:</b> {prov_name} <br />
-            <b>Municipality:</b> {mun_name} <br />
-            <b>Date:</b> {Time} <br />
-            <b>Magnitude:</b> {Magnitude} <br />
-            <b>Depth:</b> {Depth/Km} Km
-            """,
-           "style": {
-                "backgroundColor": "steelblue",
-                "color": "white"
-           }
-        }
-
-        with st.expander("**Map** 🌍", expanded=True):
-            tab1, tab2, tab3 = st.tabs(["*Points*", "*Heatmap*", "*Timelapse heatmap*"])
-
-            tab1.pydeck_chart(pdk.Deck(
-                map_provider="mapbox", 
-                map_style="road",
-                tooltip=tooltip,
-                initial_view_state=pdk.ViewState(
-                    latitude=41.902782, 
-                    longitude=12.496366,
-                    zoom=4,
-                    pitch=50,
-                ),
-                layers=[
-                    pdk.Layer(
-                        "ScatterplotLayer",
-                        data=filtered_data,
-                        pickable=True,
-                        opacity=0.3,
-                        stroked=True,
-                        filled=True,
-                        radius_scale=10,
-                        radius_min_pixels=10,
-                        radius_max_pixels=100,
-                        line_width_min_pixels=1,
-                        get_position='[Longitude, Latitude]',
-                        get_radius="Magnitude",
-                        get_fill_color=[255, 140, 0],
-                        get_line_color=[0, 0, 0],
-
-                    )
-                ],
-            ), use_container_width=True)
-            
-            #---HEATMAP---
-            m_1 = folium.Map(location=[41.902782, 12.496366],
-                            zoom_start=5)
-                
-            heat_data = [[row['Latitude'],row['Longitude']] for index, row in filtered_data.iterrows()]
-            HeatMap(heat_data,radius=8, blur=10).add_to(m_1)
-           
-
-            #---TIMELAPSE---
-            df_HeatMap = filtered_data[['Time', 'Latitude', 'Longitude']].sort_values('Time').reset_index(drop=True)
-            df_HeatMap['Time'] = df_HeatMap['Time'].astype(str)
-
-            lat_long_list = []
-            for i in df_HeatMap.Time.unique():
-                temp=[]
-                for index, instance in df_HeatMap[df_HeatMap['Time'] == i].iterrows():
-                    temp.append([instance['Latitude'],instance['Longitude']])
-                lat_long_list.append(temp)
-
-            m = folium.Map(location=[41.902782, 12.496366],
-                           zoom_start=5)
+        heat_data = [[row['Latitude'],row['Longitude']] for index, row in filtered_data.iterrows()]
+        HeatMap(heat_data,radius=8, blur=10).add_to(m_1)
 
 
-            HeatMapWithTime(lat_long_list,
-                            index=df_HeatMap.Time.unique().tolist(),
-                            name='heatmap',
-                            overlay=False,
-                            radius=15,
-                            auto_play=True,
-                            min_speed=2,
-                            max_speed =5,
-                            speed_step=1,
-                            position='bottomright',
-                            display_index=True
-                            ).add_to(m)
-            
-           
+        #---TIMELAPSE---
+        df_HeatMap = filtered_data[['Time', 'Latitude', 'Longitude']].sort_values('Time').reset_index(drop=True)
+        df_HeatMap['Time'] = df_HeatMap['Time'].astype(str)
 
-            #fullscreen
-            folium.plugins.Fullscreen(position='topleft', title='Full Screen', title_cancel='Exit Full Screen', force_separate_button=True,).add_to(m)
-            
-            with tab2:
-                folium_static(m_1)
-            with tab3:
-                folium_static(m)
+        lat_long_list = []
+        for i in df_HeatMap.Time.unique():
+            temp=[]
+            for index, instance in df_HeatMap[df_HeatMap['Time'] == i].iterrows():
+                temp.append([instance['Latitude'],instance['Longitude']])
+            lat_long_list.append(temp)
+
+        m = folium.Map(location=[41.902782, 12.496366],
+                       zoom_start=5)
+
+
+        HeatMapWithTime(lat_long_list,
+                        index=df_HeatMap.Time.unique().tolist(),
+                        name='heatmap',
+                        overlay=False,
+                        radius=15,
+                        auto_play=True,
+                        min_speed=2,
+                        max_speed =5,
+                        speed_step=1,
+                        position='bottomright',
+                        display_index=True
+                        ).add_to(m)
+
+
+
+        #fullscreen
+        folium.plugins.Fullscreen(position='topleft', title='Full Screen', title_cancel='Exit Full Screen', force_separate_button=True,).add_to(m)
+
+        with tab2:
+            folium_static(m_1)
+        with tab3:
+            folium_static(m)
