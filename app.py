2880cac48ade2cf7443ee7edacb0759fb98ce218
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2023-03-08 09:01:22 +0100

    Update app.py

--- app.py
+++ app.py
@@ -68,13 +68,13 @@ def get_data():
         st.error('Date input error', icon="🚨")
         st.stop()
 
-# st.markdown("""
-#     <style>
-#         .stApp {
-#         background: url("https://th.bing.com/th/id/R.e6b3036960aca7aa74463f3248445d4e?rik=Pa126za3CImpJQ&riu=http%3a%2f%2fgetwallpapers.com%2fwallpaper%2ffull%2f6%2f3%2fa%2f830914-earthquake-wallpapers-2197x1463-lockscreen.jpg&ehk=3qQRMr6YFUK1a%2fPHP6A6Rg0mc5y5Sm2PuIujOANK40I%3d&risl=&pid=ImgRaw&r=0");
-#         background-size: cover;
-#         }
-#     </style>""", unsafe_allow_html=True)
+st.markdown("""
+    <style>
+        .stApp {
+        background: url("https://th.bing.com/th/id/R.e6b3036960aca7aa74463f3248445d4e?rik=Pa126za3CImpJQ&riu=http%3a%2f%2fgetwallpapers.com%2fwallpaper%2ffull%2f6%2f3%2fa%2f830914-earthquake-wallpapers-2197x1463-lockscreen.jpg&ehk=3qQRMr6YFUK1a%2fPHP6A6Rg0mc5y5Sm2PuIujOANK40I%3d&risl=&pid=ImgRaw&r=0");
+        background-size: cover;
+        }
+    </style>""", unsafe_allow_html=True)
 
 
 with st.container():
