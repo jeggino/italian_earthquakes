8cd3ef0b130a5def1eb166052317cd1c67818be7
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2023-03-09 09:58:44 +0100

    Update app.py

--- app.py
+++ app.py
@@ -80,7 +80,7 @@ def get_data():
     except:
         st.error('Date input error', icon="🚨")
         st.stop()
-left,right = st.columns([1,5],gap="small")
+left,right = st.columns([1,7],gap="small")
 left.image("vhva5co7.png",use_column_width='always')
 right.subheader("""Real-time data from the [INGV Earthquake Department](http://cnt.rm.ingv.it/en) website""")
 
